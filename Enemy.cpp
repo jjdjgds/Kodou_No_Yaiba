b8110a5686003ee57c04e402bc41d3ca61d0fe3f
AkkarinJung <ths40834@ths.hal.ac.jp>
2025-10-22 20:51:17 +0900

    Enemyクラス, Game_mapクラス 作った

---
+++ Enemy.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+#include "Enemy.h"
+
+Enemy::Enemy()
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::update()
+{
+
+}
+
+void Enemy::draw() const
+{
+	Print << U"enemy" << m_HP;
+	TextureAsset(U"enemy").draw(m_Position);
+}
---
+++ Game_Map.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "Game_Map.h"
+
+Game_Map::Game_Map()
+{
+
+}
+
+Game_Map::~Game_Map()
+{
+
+}
+
+void Game_Map::update()
+{
+
+}
+
+void Game_Map::draw() const
+{
+
+}
