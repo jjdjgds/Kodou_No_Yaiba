872795feacc3e26b71df9d8e72841f98588896ea
ChenBolUo <boluo819ber@gmail.com>
2025-10-24 18:38:33 +0900

    敵アニメーション修正し 当たり判定BOX修正し 重力作り

--- Game.cpp
+++ Game.cpp
@@ -1,13 +1,10 @@
 ﻿#include "Game.hpp"
 #include "Player.hpp"
 #include "Enemy.hpp"
-<<<<<<< HEAD
 #include "Collision.hpp"
 using namespace Collision;
-
-=======
 #include "Game_Map.hpp"
->>>>>>> c494077ae074cd3093d5f1246eede207b47988ef
+
 
 Game::Game(const InitData& init)
 	: IScene{ init }
@@ -39,45 +36,20 @@ Game::Game(const InitData& init)
 	m_enemies.clear();
 	m_enemies.reserve(8);
 
-	m_enemies.emplace_back(Vec2{ 700,600 }, 80.0, 600.0, 900.0, false, Vec2{ 3,3 });
-	m_enemies.emplace_back(Vec2{ 700,100 }, 200.0, 600.0, 900.0, true, Vec2{ 5,5 });
+	m_enemies.emplace_back(Vec2{ 700,100 }, 200.0, 600.0, 900.0, true, Vec2{ 3,3 });
 }
 
 void Game::update()
 {
-<<<<<<< HEAD
-	RectF pBox(player.getPosition(), player.getScale());
-	pBox.setPos(player.getPosition()).setSize(player.getAttackRengeBox());
-
-	
-
-
-	player.update();
-<<<<<<< HEAD
-
-	for (auto& e : m_enemies) {
-
-		RectF eBox(e.getPosition(), e.getScale());
-		eBox.setPos(e.getPosition()).setSize(e.getHitbox());
-
-		if (RectToRect(pBox,eBox)) e.takeDamage(0);// ダメージを与える
-
-		e.update();
-	}
-=======
-	
-	
-	// 全ての敵キャラクターの状態を1フレーム分進める
-	for (auto& e : m_enemies) e.update(player);// 敵キャラクターを更新
->>>>>>> 72fd5ce0486a6937be8b43edcce319793524be64
-=======
 	player.update(map);
 	map.updateCamera(player.GetPlayerPosition());
 	map.update();
 
-	for (auto& e : m_enemies)
-		e.update(player);
->>>>>>> c494077ae074cd3093d5f1246eede207b47988ef
+
+
+
+	for (auto& e : m_enemies) e.update(player,map);
+
 }
 
 void Game::draw() const
@@ -87,6 +59,5 @@ void Game::draw() const
 	map.draw();                // ← マップを描画
 	player.draw();             // ← プレイヤーを描画
 
-	for (const auto& e : m_enemies)
-		e.draw();
+	for (const auto& e : m_enemies) e.draw(); //敵描画
 }
