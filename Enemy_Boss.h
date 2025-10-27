8fb0700962600ddc6a8379951a57b08955feea16
AkkarinJung <ths40834@ths.hal.ac.jp>
2025-10-28 00:19:55 +0900

    Enemy_Bosssssssssssssssssssssssssssssssssssssssssss

--- Enemy_Boss.h
+++ Enemy_Boss.h
@@ -22,7 +22,7 @@ class Enemy_Boss
 {
 private :
 	Vec2 m_boss_pos;
-	Vec2 m_boss_scal;
+	Vec2 m_boss_scale;
 
 	bool m_FaceRight;
 
@@ -38,8 +38,18 @@ private :
 	Boss_Pattern m_pattern = Boss_Pattern::PATTERN_1;
 
 public:
+	Enemy_Boss(Vec2 pos, double stride,
+			 bool faceRight, Vec2 scale)
+		: m_boss_pos(pos)
+		, m_FaceRight(faceRight)
+		, m_boss_scale(scale) {
+	}
+
+	void update(Player& player, Game_Map& map);
+	void draw(const Game_Map& CameraPos) const;
+
 	const Vec2& GetPosition() const { return m_boss_pos; }
-	const Vec2& GetScale() const { return m_boss_scal; }
+	const Vec2& GetScale() const { return m_boss_scale; }
 	int GetHP() const { return m_boss_hp; }
 	int GetBPM() const { return m_boss_bpm; }
 	int GetAttack() const { return m_boss_atk; }
@@ -48,7 +58,7 @@ public:
 	Vec2 getHitbox() const { return m_hitBox; }
 
 	void SetPosition(const Vec2& pos) { m_boss_pos = pos; }
-	void SetScale(const Vec2& scale) { m_boss_scal = scale; }
+	void SetScale(const Vec2& scale) { m_boss_scale = scale; }
 	void SetHP(int hp) { m_boss_hp = hp; }
 	void SetBPM(int bpm) { m_boss_bpm = bpm; }
 	void SetAttack(int atk) { m_boss_atk = atk; }
@@ -57,10 +67,6 @@ public:
 	void setHitbox(Vec2 hitbox) { m_hitBox = hitbox; }
 
 	Enemy_Boss& GetEnemy_Boss() { return *this; }
-
-	void update(Player& player, Game_Map& map);
-
-	void draw(const Game_Map& CameraPos) const;
 };
 
 //->hp
